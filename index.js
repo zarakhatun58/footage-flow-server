6b9bc2c540a09d51bff2b113d55dd9316f42b1b8
Jahanara Khatun <zarakhatun58@gmail.com>
2025-08-04 16:00:14 +0530

    update

--- index.js
+++ index.js
@@ -56,7 +56,10 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error('MongoDB error', err));
 
 // ✅ Serve static uploads
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(uploadsPath));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+
 app.use('/api', uploadRoutes);
 app.use('/api', storyRoutes);
 app.use('/api/auth', authRoutes);
