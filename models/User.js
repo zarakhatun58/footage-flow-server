54d8a6822d7a089df13a20e9509a2e4031c408a5
Jahanara Khatun <zarakhatun58@gmail.com>
2025-10-01 22:29:00 +0530

    modal add

--- models/User.js
+++ models/User.js
@@ -2,10 +2,12 @@ import mongoose from 'mongoose';
 
 const reelUserSchema = new mongoose.Schema({
  googleId: String,
-   email: { type: String, required: true, unique: true }, // ✅ email should be unique
+   email: { type: String, required: true, unique: true }, 
   username: String,
   profilePic: String,
-  password: String, // ← ADD THIS
+  password: String,
+  googleAccessToken: String,   
+  googleRefreshToken: String,  
   createdAt: { type: Date, default: Date.now }
 });
 export default mongoose.model('reelUser', reelUserSchema);
